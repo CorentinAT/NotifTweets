3eeace9efb2b43b017ad512d8792282e3a40cd79
Corentin ALBERT <corentin.al@free.fr>
2023-01-04 09:10:58 +0100

    mis en fonction pour l'executer avec une liste d'arobases

--- main.py
+++ main.py
@@ -2,50 +2,52 @@ from dotenv import load_dotenv
 import os
 import requests
 
-load_dotenv()
-webhook = os.getenv("webhook")
+def envoyer_timeline(abonnements:list)->None:
+    for arobase in abonnements:
+        load_dotenv()
+        webhook = os.getenv("webhook")
+        bearer_token = os.getenv("bearer_token")
 
-headers = {
-    "Authorization": "Bearer "+os.getenv("bearer_token")
-}
-
-url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
-
-params = {
-    "screen_name": "marc_le_marco",
-    "exclude_replies": True,
-    "tweet_mode": "extended",
-}
-
-response = requests.get(url, params=params, headers=headers)
-
-req = response.json()
-
-for tweet in req:
-    print(f"{tweet}\n\n")
-    media = []
-    try:
-        media = tweet["extended_entities"]["media"]
-    except:
-        pass
-
-    images = []
-    for image in media:
-        images.append(image["media_url"])
-
-    embeds = [{
-        "description": tweet["full_text"],
-        "title": tweet["user"]["name"]+f" (@{tweet['user']['screen_name']})",
-        "color": 3092479,
-        "thumbnail": {
-            "url": tweet["user"]["profile_image_url"]
-        },
-        "image": {
-            "url": images[0] if len(images)>0 else None
+        url = "https://api.twitter.com/1.1/statuses/user_timeline.json"
+        headers = {
+            "Authorization": f"Bearer {bearer_token}"
         }
-    }]
-    message = {
-        "embeds": embeds
-    }
+        params = {
+            "screen_name": arobase,
+            "exclude_replies": True,
+            "tweet_mode": "extended",
+        }
 
-    requests.post(webhook, json=message)
+        response = requests.get(url, params=params, headers=headers)
+        req = response.json()
+
+        for tweet in req:
+            print(f"{tweet}\n\n")
+            media = []
+            try:
+                media = tweet["extended_entities"]["media"]
+            except:
+                pass
+
+            images = []
+            for image in media:
+                images.append(image["media_url"])
+
+            embeds = [{
+                "description": tweet["full_text"],
+                "title": tweet["user"]["name"]+f" (@{tweet['user']['screen_name']})",
+                "color": 3092479,
+                "thumbnail": {
+                    "url": tweet["user"]["profile_image_url"]
+                },
+                "image": {
+                    "url": images[0] if len(images)>0 else None
+                }
+            }]
+            message = {
+                "embeds": embeds
+            }
+            requests.post(webhook, json=message)
+
+timeline = ["marc_le_marco", "grofou_", "ndoki94_"]
+envoyer_timeline(timeline)
